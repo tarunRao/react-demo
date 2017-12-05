64f17399b1dabcc62a78a791fbcf8b207c4ac946
Tarun Rao <Tarun.Rao@asg.com>
2017-12-05 16:31:56 +0530

    WIP-initiated react redux

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="left brand-logo">
+              React Demo
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="/auth/google">Login with Google</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    );
+  }
+}
+
+export default Header;
